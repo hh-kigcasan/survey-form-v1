b3bdf14c8961a5e8ccb8abbc26a0d263e846b2bb
Jeasson Seroy <61196026+JsonDevSolutions@users.noreply.github.com>
2023-03-27 15:29:28 +0800

    bug already fix

--- models/survey.model.js
+++ models/survey.model.js
@@ -5,7 +5,7 @@ const Constants 			= require('../config/constants');
 class SurveyModel extends Model {
 	constructor() {
 		super();
-		this.captcha = null;
+		this.captcha = this.generateCaptcha();
 	}
 
 	async getSurvey() {
@@ -46,11 +46,15 @@ class SurveyModel extends Model {
 
 	// supply the logic for each function:
 	generateCaptcha(){
-		return ""; 
+		return "generated_captcha"; 
 	}
 
 	verifyCaptchaInput(input){
-		return ""; 
+		if(input != this.captcha){
+			return "Error! Captcha input doesn't matched.";
+		}else{
+			return "Success! Captcha input matched.";
+		}
 	}
 }
 
