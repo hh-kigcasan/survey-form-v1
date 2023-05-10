92bf6af65bd2eee3c28a0050063f425cdee26815
ron029 <111740743+ron029@users.noreply.github.com>
2023-05-11 06:43:09 +0800

    captcha_generate_value

--- models/survey.model.js
+++ models/survey.model.js
@@ -5,7 +5,7 @@ const Constants 			= require('../config/constants');
 class SurveyModel extends Model {
 	constructor() {
 		super();
-		this.captcha = null;
+		this.captcha = this.generateCaptcha;
 	}
 
 	async getSurvey() {
@@ -46,7 +46,7 @@ class SurveyModel extends Model {
 
 	// supply the logic for each function:
 	generateCaptcha(){
-		return '';
+		return 'new_captcha';
 	}
 
 	verifyCaptchaInput(input){
