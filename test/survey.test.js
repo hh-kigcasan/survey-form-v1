f834f238afe0530dcb25b58b9064c839899ece6f
Froilan Fajardo <froilan012@gmail.com>
2023-07-14 10:15:52 +0800

    Jenkins testing

--- test/survey.test.js
+++ test/survey.test.js
@@ -10,9 +10,7 @@ describe("Survey Model", function(){
     it('Should return success message when input captcha matches.', function(){
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
-        let result = surveyModel.verifyCaptchaInput(captcha);
-        console.log(captcha);
-        
+        let result = surveyModel.verifyCaptchaInput(captcha);        
         expect(result).to.equal("Success! Captcha input matched.");
     });
 
