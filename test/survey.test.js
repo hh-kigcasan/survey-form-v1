6f4f880d769b4e5508fd47197dbb0631803f50ad
Brian Clark Doman <brianclark021@gmail.com>
2024-03-22 18:40:27 +0800

    minor commnet for jenkins test

--- test/survey.test.js
+++ test/survey.test.js
@@ -6,7 +6,6 @@ const SurveyModel           = require('../models/survey.model');
 
 
 describe("Survey Model", function(){
-    // hello
     it('Should return success message when input captcha matches.', function(){
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
